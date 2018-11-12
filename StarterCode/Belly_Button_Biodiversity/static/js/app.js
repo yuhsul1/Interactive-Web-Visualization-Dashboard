329890b509b13106acb85b17717b2cbd11f578d1
Peter Liu <yuhsul1@uci.edu>
2018-11-11 18:02:59 -0800

    APP ready for Heroku Deployment

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -1,4 +1,24 @@
+
+
 function buildMetadata(sample) {
+  let url = "/metadata/"+ sample
+  d3.json(url).then(function(response){
+    console.log(response)
+    var sampleMetadata = d3.select("#sample-metadata");
+    sampleMetadata.html("")
+    Object.entries(response).forEach(function ([key, value]) {
+      var row = sampleMetadata.append("p");
+      row.text(`${key}: ${value}`);
+  
+      })
+    }
+  )
+  d3.select("#sample-metadata")
+
+
+  
+  
+  
 
   // @TODO: Complete the following function that builds the metadata panel
 
@@ -16,14 +36,94 @@ function buildMetadata(sample) {
 }
 
 function buildCharts(sample) {
+  var url = `/samples/${sample}`;
 
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
+  //Organize raw data into array of objects
+  d3.json(url).then(function(data) {
+    console.log(data)
+    
+    let limit = data.sample_values.length 
+    let patientInfo=[]
+    for (i=0; i<limit; i++) {
+      patientInfo.push({
+        "Label": data.otu_labels[i],
+        "ID": data.otu_ids[i],
+        "Value":data.sample_values[i]
+      })
+    }
 
-    // @TODO: Build a Bubble Chart using the sample data
+  //sort the array of objects by the sample Value
+    patientInfo.sort(function compareFunction(first, second){
+      return second.Value-first.Value
+    })
 
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
+  //collect the top ten values
+    topBacteria = patientInfo.slice(0,10)
+    console.log(topBacteria)
+
+    //preparing array for pie data
+    var value = []
+    var label = []
+    var ids = []
+
+    let long = topBacteria.length
+    for (i = 0; i<long ; i++) {
+      value.push(topBacteria[i].Value)
+      label.push(topBacteria[i].Label)
+      ids.push(topBacteria[i].ID)
+    }
+    //Setting up pie chart params
+    var trace = {
+      values: value,
+      labels: ids,
+      hovertext: label,
+      type: 'pie'
+    }
+    var pieData = [trace];
+    var layout = {
+      title: "Pie Chart"
+    };
+    
+
+    Plotly.newPlot('pie', pieData, layout);
+
+    //Bubble Chart
+    //preparing array for bubble data
+    var bubbleId=[]
+    var bubbleValue=[]
+    var markerSize=[]
+    var markerColors=[]
+    var markerLabel=[]
+  
+    let loopLength = patientInfo.length
+    for (i = 0; i<loopLength ; i++) {
+      bubbleId.push(patientInfo[i].ID)
+      bubbleValue.push(patientInfo[i].Value)
+      markerSize.push(patientInfo[i].Value)
+      markerColors.push(patientInfo[i].ID)
+      markerLabel.push(patientInfo[i].Label)
+    }
+    //Setting up bubble chart params
+    var trace1 = {
+      x: bubbleId,
+      y: bubbleValue,
+      text: markerLabel,
+      mode: 'markers',
+      marker: {
+        color: markerColors,
+        size: markerSize
+      } 
+    };
+  
+    var bubbleData = [trace1];
+    var layout = {
+      xaxis: { title: "OTU ID"},
+    };
+  
+    Plotly.newPlot('bubble', bubbleData, layout);
+  
+
+  });
 }
 
 function init() {
