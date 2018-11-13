7cb9d199ac50f6c64212d30f0af6563ae655fd6b
Liu <GLS06807@hke.local>
2018-11-13 11:51:58 -0800

    gauge chart updated

--- Belly_Button_Biodiversity/static/js/app.js
+++ Belly_Button_Biodiversity/static/js/app.js
@@ -9,17 +9,90 @@ function buildMetadata(sample) {
     Object.entries(response).forEach(function ([key, value]) {
       var row = sampleMetadata.append("p");
       row.text(`${key}: ${value}`);
-  
+      
       })
-    }
+    console.log(response.WFREQ)
+
+    var washFreq = response.WFREQ
+    if (washFreq <1) {var degrees = 180-0}
+    else if (washFreq <2) {var degrees = 180-20}
+    else if (washFreq <3) {var degrees = 180- 40}
+    else if (washFreq <4) {var degrees = 180- 60}
+    else if (washFreq <5) {var degrees = 180- 80}
+    else if (washFreq <6) {var degrees = 180- 100}
+    else if (washFreq <7) {var degrees = 180- 120}
+    else if (washFreq <8) {var degrees = 180- 140}
+    else if (washFreq <9) {var degrees = 180- 160}
+    else {var degrees = 180- 180};
+
+
+
+    // Trig to calc meter point
+    var radius = .5;
+    var radians = degrees * Math.PI / 180;
+    var x = radius * Math.cos(radians);
+    var y = radius * Math.sin(radians);
+
+    // Path: may have to change to create a better triangle
+    var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+        pathX = String(x),
+        space = ' ',
+        pathY = String(y),
+        pathEnd = ' Z';
+    var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+    var data = [{ type: 'scatter',
+      x: [0], y:[0],
+        marker: {size: 28, color:'850000'},
+        showlegend: false,
+        name: 'speed',
+        text: level,
+        hoverinfo: 'text+name'},
+      { values: [10/9, 10/9, 10/9, 10/9, 10/9, 10/9, 10/9, 10/9, 10/9, 10],
+      rotation: 90,
+      text: ['8-9', '7-8', '6-7', '5-6',
+                '4-5', '3-4', '2-3','1-2','0-1',''],
+      textinfo: 'text',
+      textposition:'inside',
+      marker: {colors:['rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+                            'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+                            'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+                            'rgba(242, 226, 202, .5)','rgba(252, 246, 202, .5)',
+                            'rgba(255, 255, 202, .5)','rgba(255, 255, 255, 0)']},
+      labels: ['8-9 times', '7-8 times', '6-7 times', '5-6 times', '4-5 times', '3-4 times',
+                '2-3 times', '1-2 times', '0-1 times', ''],
+      hoverinfo: 'label',
+      hole: .5,
+      type: 'pie',
+      showlegend: false
+    }];
+
+    var layout = {
+      shapes:[{
+          type: 'path',
+          path: path,
+          fillcolor: '850000',
+          line: {
+            color: '850000'
+          }
+        }],
+      title: 'Bellybutton Scrubing Frequency', 
+      Speed: 0-100,
+      height: 500,
+      width: 500,
+      xaxis: {zeroline:false, showticklabels:false,
+                showgrid: false, range: [-1, 1]},
+      yaxis: {zeroline:false, showticklabels:false,
+                showgrid: false, range: [-1, 1]}
+    };
+
+    Plotly.newPlot('gauge', data, layout);
+
+
+        }
   )
   d3.select("#sample-metadata")
-
-
   
-  
-  
-
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
@@ -33,7 +106,19 @@ function buildMetadata(sample) {
 
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
+  // Enter a speed between 0 and 180
+
+
+
+
 }
+
+
+
+
+
+
+
 
 function buildCharts(sample) {
   var url = `/samples/${sample}`;
--- Belly_Button_Biodiversity/static/js/bonus.js
+++ Belly_Button_Biodiversity/static/js/bonus.js
@@ -1,1 +1,65 @@
 
+// Enter a speed between 0 and 180
+var level = 175;
+
+// Trig to calc meter point
+var degrees = 180 - level,
+     radius = .5;
+var radians = degrees * Math.PI / 180;
+var x = radius * Math.cos(radians);
+var y = radius * Math.sin(radians);
+
+// Path: may have to change to create a better triangle
+var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+     pathX = String(x),
+     space = ' ',
+     pathY = String(y),
+     pathEnd = ' Z';
+var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+var data = [{ type: 'scatter',
+   x: [0], y:[0],
+    marker: {size: 28, color:'850000'},
+    showlegend: false,
+    name: 'speed',
+    text: level,
+    hoverinfo: 'text+name'},
+  { values: [10/9, 10/9, 10/9, 10/9, 10/9, 10/9, 10/9, 10/9, 10/9, 10],
+  rotation: 90,
+  text: ['8-9', '7-8', '6-7', '5-6',
+            '4-5', '3-4', '2-3','1-2','0-1',''],
+  textinfo: 'text',
+  textposition:'inside',
+  marker: {colors:['rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+                         'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+                         'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+                         'rgba(242, 226, 202, .5)','rgba(252, 246, 202, .5)',
+                         'rgba(255, 255, 202, .5)','rgba(255, 255, 255, 0)']},
+  labels: ['8-9 times', '7-8 times', '6-7 times', '5-6 times', '4-5 times', '3-4 times',
+            '2-3 times', '1-2 times', '0-1 times', ''],
+  hoverinfo: 'label',
+  hole: .5,
+  type: 'pie',
+  showlegend: false
+}];
+
+var layout = {
+  shapes:[{
+      type: 'path',
+      path: path,
+      fillcolor: '850000',
+      line: {
+        color: '850000'
+      }
+    }],
+  title: 'Bellybutton Scrubing Frequency', 
+  Speed: 0-100,
+  height: 500,
+  width: 500,
+  xaxis: {zeroline:false, showticklabels:false,
+             showgrid: false, range: [-1, 1]},
+  yaxis: {zeroline:false, showticklabels:false,
+             showgrid: false, range: [-1, 1]}
+};
+
+Plotly.newPlot('gauge', data, layout);
